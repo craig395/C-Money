3c9f0590b3d815b022b17e475df3707c5bb60134
Craig Mosey <craig.mosey@gmail.com>
2015-12-06 21:50:06 +0000

    Added currency index and unit symbol object types

--- Money/Money/Currency.cpp
+++ Money/Money/Currency.cpp
@@ -21,7 +21,6 @@ public:
 };
 
 //Unit name data type
-
 class unitName {//TODO: add info on usage
 private:
 	std::string major;
@@ -30,6 +29,27 @@ public:
 	unitName(std::string majorUnitName, std::string minorUnitName) {
 		major = majorUnitName;
 		minor = minorUnitName;
+	}
+
+	std::string getMajor() {//TODO: add info on usage
+		return major;
+	}
+
+	std::string getMinor() {//TODO: add info on usage
+		return minor;
+	}
+};
+
+
+//Unit symbol data type
+class unitSymbol {//TODO: add info on usage
+private:
+	std::string major;
+	std::string minor;
+public:
+	unitSymbol(std::string majorunitSymbol, std::string minorunitSymbol) {
+		major = majorunitSymbol;
+		minor = minorunitSymbol;
 	}
 
 	std::string getMajor() {//TODO: add info on usage
@@ -71,7 +91,12 @@ private:
 	int displayDecimals;
 	displayFormat format = displayFormat(',', '.');
 public:
-	currency(std::string name, ISO4217 currencyISO, unitName currencyUnitName, int displayDecimals, displayFormat format) {
+	//Default currency type (GBP)
+	currency() {
+		//TODO: insert default details for GBP
+	}
+
+	currency(std::string name, ISO4217 currencyISO, unitName currencyUnitName, unitSymbol currencyUnitSymbol, int displayDecimals, displayFormat format) {
 		this->name = name;
 		currISO = currencyISO;
 		currUnitName = currencyUnitName;
@@ -97,5 +122,64 @@ public:
 
 	displayFormat* getFormat() {//TODO: add info on usage
 		return &format;
+	}
+};
+
+class currencyIndex {//TODO: maybe split into index template class and this
+public:
+	//Creates the initial index with a list of currencies
+	currencyIndex() {
+		insertNewCurrency(currency("Pound Sterling", ISO4217("GBP", "826"), unitName("pound", "pence"), unitSymbol("£","p"), 2, displayFormat(',', '.')));
+		//TODO: insert many more currency's
+	}
+
+	//Add's a new currency to the index.
+	//insertNewCurrency((currency) the new currency object to insert in the index), this returns to values
+	void insertNewCurrency(currency newCurrecny) {
+		//Resize array if necessary
+		if (currencyCount >= arraySize) {
+			increaseArraySize();
+		}
+
+		//Add the new currency
+		currencyArray[currencyCount] = newCurrecny;
+		//Update the currency count
+		currencyCount++;
+	}
+private:
+	const static int initialSize = 50; //Initial size of the array, by default its the number of default items plus 5
+	const static int arraySizeStep = 10; //The amount to increase the array size by
+
+	int arraySize = initialSize;
+	int currencyCount = 0;
+	currency* currencyArray = new currency[initialSize];
+
+	//Increases the size of the array
+	void increaseArraySize() {
+		int newArraySize = arraySize + arraySizeStep;
+		//TODO: maybe a check for overflow/ check tmpArray for errors.
+		//create temporary array to store current items
+		currency* tmpArray = new currency[arraySize];
+
+		//Add current items to this array
+		for (int i = 0; i < currencyCount; i++) {
+			tmpArray[i] = currencyArray[i];
+		}
+
+		//Destroy everything in the currency array
+		delete[] currencyArray;
+		//Set the currency's new size
+		currencyArray = new currency[newArraySize];
+		
+		//Refill the current array
+		for (int i = 0; i < currencyCount; i++) {
+			currencyArray[i] = tmpArray[i];
+		}
+
+		//Clean up tmpArray
+		delete[] tmpArray;
+
+		//Update the array size
+		arraySize = newArraySize;
 	}
 };
